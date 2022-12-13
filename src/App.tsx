2c64a20cb6294e8b2ca3fd6efb2d1037fa1bab06
danielpqb <danielpqb@gmail.com>
2022-12-13 00:28:36 -0300

    Configuring ESLint and removing prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,54 @@
-import { ReactNode, useEffect, useMemo } from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import styled from "styled-components";
+import { ReactNode, useEffect, useMemo } from "react"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import styled from "styled-components"
 
-import Alert from "./components/common/Alert/Alert";
-import Counter from "./components/common/Counter/Counter";
-import ProtectedRoute from "./components/common/ProtectedRoute";
+import Alert from "./components/common/Alert/Alert"
+import Counter from "./components/common/Counter/Counter"
+import ProtectedRoute from "./components/common/ProtectedRoute"
 
-import { requestUserData } from "./global-functions";
-import { useAppContext } from "./contexts/AppContext";
-import Home from "./components/pages/Home";
+import { requestUserData } from "./global-functions"
+import { useAppContext } from "./contexts/AppContext"
+import Home from "./components/pages/Home"
 
 export default function App() {
-  const { userData, setUserData, alert, counter } = useAppContext();
+  const { userData, setUserData, alert, counter } = useAppContext()
 
   useMemo(() => {
-    const headerColor = "color: #ffbe88; font-weight: bold;";
-    const paramColor = "\x1B[38;2;126;204;152m"; //rgb(126, 204, 152)"
-    const dependencyColor = "\x1B[38;2;255;154;136m"; //rgb(255, 154, 136)"
-    const stringColor = "\x1B[38;2;54;206;193m"; //rgb(54, 206, 193)"
-    const resetColor = "\x1B[m";
+    const headerColor = "color: #ffbe88; font-weight: bold;"
+    const paramColor = "\x1B[38;2;126;204;152m" //rgb(126, 204, 152)"
+    const dependencyColor = "\x1B[38;2;255;154;136m" //rgb(255, 154, 136)"
+    const stringColor = "\x1B[38;2;54;206;193m" //rgb(54, 206, 193)"
+    const resetColor = "\x1B[m"
 
     const separateParams = (obj: object) => {
       if (obj) {
         Object.entries(obj).forEach((keyValuePair) => {
           if (typeof keyValuePair[1] === "string") {
-            console.log(`  ${paramColor}${keyValuePair[0]}: ${stringColor}'${keyValuePair[1]}'`);
-            return;
+            console.log(
+              `  ${paramColor}${keyValuePair[0]}: ${stringColor}'${keyValuePair[1]}'`
+            )
+            return
           }
-          console.log(`  ${paramColor}${keyValuePair[0]}:`, keyValuePair[1]);
-        });
+          console.log(`  ${paramColor}${keyValuePair[0]}:`, keyValuePair[1])
+        })
       }
-      console.log("\n ");
-    };
+      console.log("\n ")
+    }
 
-    console.clear();
-    console.log(`%c\nuserData ${dependencyColor}(dependency)${resetColor}`, headerColor);
-    separateParams(userData);
-  }, [userData]);
+    console.clear()
+    console.log(
+      `%c\nuserData ${dependencyColor}(dependency)${resetColor}`,
+      headerColor
+    )
+    separateParams(userData)
+  }, [userData])
 
   useEffect(() => {
-    const localToken = localStorage.getItem("userToken");
+    const localToken = localStorage.getItem("userToken")
     if (localToken) {
-      requestUserData(localToken, setUserData);
+      requestUserData(localToken, setUserData)
     }
-  }, [setUserData]);
+  }, [setUserData])
 
   return (
     <>
@@ -53,25 +58,23 @@ export default function App() {
       <Background>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route
-              path="/"
-              element={<Home />}
-            />
-            <Route element={<ProtectedRoute token={localStorage.getItem("userToken")} />}>
-              <Route
-                path="*"
-                element={<Navigate to="/" />}
-              />
+              element={
+                <ProtectedRoute token={localStorage.getItem("userToken")} />
+              }
+            >
+              <Route path="*" element={<Navigate to="/" />} />
             </Route>
           </Routes>
         </BrowserRouter>
       </Background>
     </>
-  );
+  )
 }
 
 function Background({ children }: { children: ReactNode }) {
-  return <Layer1>{children}</Layer1>;
+  return <Layer1>{children}</Layer1>
 }
 
 const Layer1 = styled.div`
@@ -86,4 +89,4 @@ const Layer1 = styled.div`
       padding: 0px;
     }
   }
-`;
+`
--- src/global-functions.ts
+++ src/global-functions.ts
@@ -5,16 +5,16 @@ import { UserData } from "./global-types";
 import { useAlertOnRequestError } from "./components/common/Alert/hooks/useAlertOnRequestError";
 
 export function requestUserData(
-  localToken: string,
-  setUserData: React.Dispatch<React.SetStateAction<Partial<UserData>>>
+	localToken: string,
+	setUserData: React.Dispatch<React.SetStateAction<Partial<UserData>>>
 ) {
-  promiseRetry((retry, number) => {
-    return getUserDataByToken(localToken).catch(retry);
-  }).then((res: Response) => {
-    delete res.data.message;
-    setUserData(res.data);
-  }),
-    (error: Error) => {
-      useAlertOnRequestError(error);
-    };
+	promiseRetry((retry, number) => {
+		return getUserDataByToken(localToken).catch(retry);
+	}).then((res: Response) => {
+		delete res.data.message;
+		setUserData(res.data);
+	}),
+	(error: Error) => {
+		useAlertOnRequestError(error);
+	};
 }
