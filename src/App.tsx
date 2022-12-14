14fcfe12f2ade307bc5448a250c93742715c725f
danielpqb <danielpqb@gmail.com>
2022-12-14 11:56:59 -0300

    Organizing files

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,19 @@ import { ReactNode, useEffect, useMemo } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import styled from "styled-components";
 
-import Alert from "./components/common/Alert/Alert";
 import Counter from "./components/common/Counter/Counter";
 import ProtectedRoute from "./components/common/ProtectedRoute";
 
 import { requestUserData } from "./global-functions";
 import { useAppContext } from "./contexts/AppContext";
 import Home from "./components/pages/Home";
+import { useConsoleLogVariables } from "global-hooks";
+import Alert from "components/common/Alert/Alert";
 
 export default function App() {
-  const { userData, setUserData, alert, counter } = useAppContext();
+  const { setUserData, alert, counter } = useAppContext();
 
-  useMemo(() => {
-    const headerColor = "color: #ffbe88; font-weight: bold;";
-    const paramColor = "\x1B[38;2;126;204;152m"; //rgb(126, 204, 152)"
-    const dependencyColor = "\x1B[38;2;255;154;136m"; //rgb(255, 154, 136)"
-    const stringColor = "\x1B[38;2;54;206;193m"; //rgb(54, 206, 193)"
-    const resetColor = "\x1B[m";
-
-    const separateParams = (obj: object) => {
-      if (obj) {
-        Object.entries(obj).forEach((keyValuePair) => {
-          if (typeof keyValuePair[1] === "string") {
-            console.log(
-              `  ${paramColor}${keyValuePair[0]}: ${stringColor}'${keyValuePair[1]}'`
-            );
-            return;
-          }
-          console.log(`  ${paramColor}${keyValuePair[0]}:`, keyValuePair[1]);
-        });
-      }
-      console.log("\n ");
-    };
-
-    console.clear();
-    console.log(
-      `%c\nuserData ${dependencyColor}(dependency)${resetColor}`,
-      headerColor
-    );
-    separateParams(userData);
-  }, [userData]);
+  useConsoleLogVariables();
 
   useEffect(() => {
     const localToken = localStorage.getItem("userToken");
